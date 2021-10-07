99aa7984bdd66918433cba24beab96d846195bed
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-10-07 14:06:58 +0200

    added counter function and store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented } from './features/counter/counter-slice';
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  function handleClick() {
+    dispatch(incremented());
+  }
 
   return (
     <div className="App">
@@ -11,31 +18,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={handleClick}>
             count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
