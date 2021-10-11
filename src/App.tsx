40b7494d8034f0e83f5cb8515a8399ca4007e719
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-10-11 16:30:00 +0200

    added api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { incremented } from './features/counter/counter-slice';
+import { useFetchBreedsQuery } from './features/dogs/dogs-api-slice'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const { data = [], isFetching} = useFetchBreedsQuery();
 
   function handleClick() {
     dispatch(incremented());
@@ -22,6 +24,31 @@ function App() {
             count is: {count}
           </button>
         </p>
+        <div>
+          <p>
+            Number of dogs fetched: {data.length}
+          </p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height={250}/>
+                  </td>
+                </tr>
+
+              ))}
+            </tbody>
+          </table>
+
+        </div>
       </header>
     </div>
   )
